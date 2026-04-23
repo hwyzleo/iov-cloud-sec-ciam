f463bfda20301b269b5933fb8de0008218f5dc41
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-04-23 16:59:33 +0800

    改进: 移除UserSearchResponse中的JSON属性映射注解

--- sec-ciam-service/src/main/java/net/hwyz/iov/cloud/sec/ciam/service/adapter/web/vo/UserSearchResponse.java
+++ sec-ciam-service/src/main/java/net/hwyz/iov/cloud/sec/ciam/service/adapter/web/vo/UserSearchResponse.java
@@ -15,35 +15,23 @@ import java.time.Instant;
 @AllArgsConstructor
 public class UserSearchResponse {
 
-    @JsonProperty("user_id")
     private String userId;
 
-    @JsonProperty("user_status")
     private Integer userStatus;
 
-    @JsonProperty("register_source")
     private String registerSource;
 
-    @JsonProperty("register_channel")
     private String registerChannel;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @JsonProperty("last_login_time")
     private Instant lastLoginTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @JsonProperty("create_time")
     private Instant createTime;
 
-    @JsonProperty("nickname")
     private String nickname;
 
-    @JsonProperty("gender")
     private Integer gender;
 
-    @JsonProperty("identity_type")
     private String identityType;
 
-    @JsonProperty("identity_value")
     private String identityValue;
 }
